fed9af5fc96e615ecb751c5e6d1dbb7bf9b99914
Shahar Shalev <shaharshalev0@gmail.com>
2018-07-29 17:33:49 +0300

    remove port from .env.example

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,14 +1,17 @@
 const path = require('path');
+const dotenvSafe = require('dotenv-safe');
+
+dotenvSafe.config({allowEmptyValues:true});
 
 // import .env variables
-require('dotenv-safe').load({
+dotenvSafe.load({
   path: path.join(__dirname, '../../.env'),
   sample: path.join(__dirname, '../../.env.example'),
 });
 
 module.exports = {
   env: process.env.NODE_ENV,
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
   mongo: {
